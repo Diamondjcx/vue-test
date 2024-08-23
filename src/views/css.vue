6c64e148d7e56f5092bef15d5457d6d5e608c7ba
“jiaochunxue” <jiaochunxue@datatang.com>
2024-08-23 10:09:51 +0800

    css垂直居中链接

--- src/views/css.vue
+++ src/views/css.vue
@@ -1,6 +1,9 @@
 <template>
   <h1>css新特性 垂直居中对齐</h1>
   <div class="layout">这是一段文字</div>
+  <a href="https://build-your-own.org/blog/20240813_css_vertical_center/"
+    >链接</a
+  >
 </template>
 
 <script setup>
