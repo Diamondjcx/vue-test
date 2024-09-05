5caebc90792d7375ad071c9b97df37bc6fc0a53d
“jiaochunxue” <jiaochunxue@datatang.com>
2024-09-05 11:49:26 +0800

    css新特性

---
+++ src/views/css/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>css新特性 垂直居中对齐</h1>
+  <div class="layout">这是一段文字</div>
+  <a href="https://build-your-own.org/blog/20240813_css_vertical_center/"
+    >链接</a
+  >
+</template>
+  
+  <script setup>
+</script>
+  
+  <style  scoped>
+.layout {
+  width: 200px;
+  height: 200px;
+  border: 1px solid pink;
+  margin: 10px;
+  text-align: center;
+  align-content: center;
+}
+</style>
