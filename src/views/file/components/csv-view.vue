c0060d344eac3cba59815587e1d69c1f70473031
“jiaochunxue” <jiaochunxue@datatang.com>
2024-12-10 17:51:21 +0800

    feat:file-view

---
+++ src/views/file/components/csv-view.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <a :href="src">data-table.csv</a>
+  </div>
+</template>
+
+<script setup>
+const src =
+  "http://10.10.15.16:18000/api/v1/proxy/user/file/comm/download?filename=%2Fdisk02%2Fatti%2Fdata%2Fdata%2FProviderying1%2F57574367%2Fex%2F%2Fdata-table.csv&node=10.10.15.21";
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/file/index.vue
+++ src/views/file/index.vue
@@ -6,12 +6,16 @@
     <n-tab-pane name="excel" title="Excel">
       <ExcelView />
     </n-tab-pane>
+    <n-tab-pane name="csv" title="Csv">
+      <csvView />
+    </n-tab-pane>
   </n-tabs>
 </template>
 
 <script setup>
 import DocView from "./components/doc-view.vue";
 import ExcelView from "./components/excel-view.vue";
+import csvView from "./components/csv-view.vue";
 </script>
 
 <style lang="scss" scoped>
